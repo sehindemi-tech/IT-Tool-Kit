b044127baa3e39a8b2f6bac3609c94f8b9fc645a
Sehindemi Ajayi <sehindemi.ajayi@gosh.org>
2026-06-27 14:59:38 +0100

    added s3 bucket  policies for iam role

--- bootstrap/locals.tf
+++ bootstrap/locals.tf
@@ -6,6 +6,7 @@ locals {
       force_destroy = false
       sse_algorithm = "AES256"
     }
+
     tfstate = {
       desription    = "Main s3 bucket terraform state"
       versioning    = true
@@ -21,18 +22,24 @@ locals {
       description   = "GitHub Actions Plan role"
       sub_condition = "StringLike"
       sub_value     = "repo:${var.project_settings.org}/${var.project_settings.github_repo}:*"
+      s3_actions    = ["s3:GetObject", "s3:ListBucketVersions", "s3:ListBucket", "s3:GetBucketVersioning"]
+      s3_buckets    = ["tfstate"]
     }
 
     apply = {
       description   = "GitHub Actions Apply role"
-      sub_condition = "StringEqual"
+      sub_condition = "StringEquals"
       sub_value     = "repo:${var.project_settings.org}/${var.project_settings.github_repo}:ref:refs/heads/main"
+      s3_actions    = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning"]
+      s3_buckets    = ["tfstate"]
     }
 
     destroy = {
       description   = "GitHub Actions Destroy role"
-      sub_condition = "StringEqual"
+      sub_condition = "StringEquals"
       sub_value     = "repo:${var.project_settings.org}/${var.project_settings.github_repo}:ref:refs/heads/main"
+      s3_actions    = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket", "s3:GetBucketVersioning"]
+      s3_buckets    = ["tfstate"]
     }
   }
 }
